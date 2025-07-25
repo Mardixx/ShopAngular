192b7e4efc19cd2a66fe290bb1044cb8c85d2441
Mardixx <kimi.lefort.pro@gmail.com>
2025-07-25 11:38:02 +0200

    added post of articles

--- src/app/shared/components/post-article/post-article.component.ts
+++ src/app/shared/components/post-article/post-article.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RESTApiService } from '../../services/restapi.service';
 
 @Component({
   selector: 'app-post-article',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './post-article.component.html',
-  styleUrl: './post-article.component.scss'
+  styleUrl: './post-article.component.scss',
+  standalone: true
 })
 export class PostArticleComponent {
+  constructor( private restAPI: RESTApiService ) {}
 
+  postInfo: any = new FormGroup({
+    name: new FormControl(''),
+    shortDescription: new FormControl(''),
+    longDescription: new FormControl(''),
+    typeOfObject: new FormControl(''),
+    price: new FormControl('')
+  })
+
+
+  postForm() {    
+    this.restAPI.postArticle(this.postInfo.value).subscribe(() => {
+      console.log('Post succeful');
+      return window.location.reload();
+    })
+  }
 }
--- src/app/shared/services/restapi.service.ts
+++ src/app/shared/services/restapi.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,9 @@ export class RESTApiService {
   public updateArticle(updatedObject: Object, id: number) {
     let url = "http://localhost:8080/articles/" + id;
     return this.http.put(url, updatedObject);
+  }
+  public postArticle(postInformation: any) {
+    let url = "http://localhost:8080/articles";
+    return this.http.post(url, postInformation);
   }
 }
